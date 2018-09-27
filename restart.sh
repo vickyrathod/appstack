0b42d68e9f3b34c10e8d015a9c138d035360fc5d
vickyrathod <vickyrathod055@gmail.com>
2018-09-27 01:01:38 -0700

    script corrected

--- restart.sh
+++ restart.sh
@@ -1,3 +1,9 @@
+alias docker-compose="'"'docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:/rootfs/$PWD" \
+    -w="/rootfs/$PWD" \
+    docker/compose:1.13.0'"'"
+
 docker-compose down
 docker-compose rm
 docker-compose up
